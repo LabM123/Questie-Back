468135a75cf211aab347d5ac214209ef18ac11c0
Joel Blanco <joeld.blanco@gmail.com>
2024-05-05 21:33:59 -0500

    feat: Add modules for users, products, categories, courses, lessons, contents, invoices, stats, and enrolments as well as TypeORM config file to App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { CoursesModule } from './modules/courses/courses.module';
+import { ModulesModule } from './modules/modules/modules.module';
+import { LessonsModule } from './modules/lessons/lessons.module';
+import { ContentsModule } from './modules/contents/contents.module';
+import { InvoicesModule } from './modules/invoices/invoices.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+import { StatsModule } from './modules/stats/stats.module';
+import { EnrolmentsModule } from './modules/enrolments/enrolments.module';
+import typeOrmConfig from './config/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm') as DataSourceOptions,
+    }),
+    UsersModule,
+    ProductsModule,
+    CategoriesModule,
+    CoursesModule,
+    ModulesModule,
+    LessonsModule,
+    ContentsModule,
+    InvoicesModule,
+    StatsModule,
+    EnrolmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
