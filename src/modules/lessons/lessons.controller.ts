c6f567b1e6ca4b73aa55a2fa1ead9d86ae5f38aa
Joel Blanco <joeld.blanco@gmail.com>
2024-05-05 21:31:34 -0500

    feat: Add lessons module with DTOs, entities, controller, and service

---
+++ src/modules/lessons/lessons.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { LessonsService } from './lessons.service';
+import { CreateLessonDto } from './dto/create-lesson.dto';
+import { UpdateLessonDto } from './dto/update-lesson.dto';
+
+@Controller('lessons')
+export class LessonsController {
+  constructor(private readonly lessonsService: LessonsService) {}
+
+  @Post()
+  create(@Body() createLessonDto: CreateLessonDto) {
+    return this.lessonsService.create(createLessonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.lessonsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.lessonsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateLessonDto: UpdateLessonDto) {
+    return this.lessonsService.update(+id, updateLessonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.lessonsService.remove(+id);
+  }
+}
