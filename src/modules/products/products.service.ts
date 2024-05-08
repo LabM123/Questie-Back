de6cc8fcaaee7f69f9c08529ed60c3cb6d056082
Joel Blanco <joeld.blanco@gmail.com>
2024-05-08 18:36:54 -0500

    feat: Add admin endpoint to retrieve all products, including deleted ones
    This commit adds a new endpoint `/admin` to the `ProductsController` that allows retrieving all products, including the deleted ones. It also updates the `findAll` method in the `ProductsService` to accept an optional parameter `withDeleted` to control whether to include deleted products in the result.

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
-import { UpdateResult } from 'typeorm';
 import { Course } from '../courses/entities/course.entity';
 import { Module } from '../modules/entities/module.entity';
 import { Content } from '../contents/entities/content.entity';
@@ -39,23 +42,23 @@ export class ProductsService {
       let entity = null;
       switch (polymorphicEntityType) {
         case 'course':
-          entity = await this.courseRepository.findOneBy({
-            id: polymorphicEntityId,
+          entity = await this.courseRepository.findOne({
+            where: { id: polymorphicEntityId },
           });
           break;
         case 'module':
-          entity = await this.moduleRepository.findOneBy({
-            id: polymorphicEntityId,
+          entity = await this.moduleRepository.findOne({
+            where: { id: polymorphicEntityId },
           });
           break;
         case 'lesson':
-          entity = await this.lessonRepository.findOneBy({
-            id: polymorphicEntityId,
+          entity = await this.lessonRepository.findOne({
+            where: { id: polymorphicEntityId },
           });
           break;
         case 'content':
-          entity = await this.contentRepository.findOneBy({
-            id: polymorphicEntityId,
+          entity = await this.contentRepository.findOne({
+            where: { id: polymorphicEntityId },
           });
           break;
       }
@@ -70,12 +73,15 @@ export class ProductsService {
     return this.productRepository.save(newProduct);
   }
 
-  async findAll(): Promise<Product[]> {
-    return await this.productRepository.find();
+  async findAll(withDeleted: boolean = false): Promise<Product[]> {
+    return await this.productRepository.find({ withDeleted });
   }
 
-  async findOne(id: string): Promise<Product> {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(id: string, withDeleted: boolean = false): Promise<Product> {
+    const product = await this.productRepository.findOne({
+      where: { id },
+      withDeleted,
+    });
 
     if (!product) {
       throw new NotFoundException(`Product not found`);
@@ -87,28 +93,40 @@ export class ProductsService {
   async update(
     id: string,
     updateProductDto: UpdateProductDto,
-  ): Promise<UpdateResult> {
-    const product = await this.productRepository.findOneBy({ id });
+  ): Promise<{ id: string }> {
+    const product = await this.productRepository.findOne({ where: { id } });
 
     if (!product) {
       throw new NotFoundException(`Product not found`);
     }
 
-    return await this.productRepository.update(id, {
-      ...product,
-      ...updateProductDto,
-    });
+    const updatedProduct = await this.productRepository.update(
+      id,
+      updateProductDto,
+    );
+
+    if (updatedProduct.affected <= 0) {
+      throw new InternalServerErrorException(`Product not updated`);
+    }
+
+    return { id };
   }
 
-  async remove(id: string): Promise<UpdateResult> {
-    const product = this.productRepository.findOneBy({ id });
+  async remove(id: string): Promise<{ id: string }> {
+    const product = this.productRepository.findOne({ where: { id } });
 
     if (!product) {
       throw new NotFoundException(`Product not found`);
     }
 
-    return await this.productRepository.update(id, {
+    const deletedProduct = await this.productRepository.update(id, {
       deleted_at: new Date(),
     });
+
+    if (deletedProduct.affected <= 0) {
+      throw new InternalServerErrorException(`Product not deleted`);
+    }
+
+    return { id };
   }
 }
