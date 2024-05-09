0e78699b461e6a635c4cd0ccdf32cdcfc6ef5148
Joel Blanco <joeld.blanco@gmail.com>
2024-05-09 13:13:56 -0500

    [feat/enrolments]: Implemented enrolments module
    This commits implements enrolments' module, including DTOs, controller and service.

--- src/modules/enrolments/enrolments.service.ts
+++ src/modules/enrolments/enrolments.service.ts
@@ -1,26 +1,127 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateEnrolmentDto } from './dto/create-enrolment.dto';
 import { UpdateEnrolmentDto } from './dto/update-enrolment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Enrolment } from './entities/enrolment.entity';
+import { Repository } from 'typeorm/repository/Repository';
+import { Course } from '../courses/entities/course.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class EnrolmentsService {
-  create(createEnrolmentDto: CreateEnrolmentDto) {
-    return 'This action adds a new enrolment';
+  constructor(
+    @InjectRepository(Enrolment)
+    private enrolmentRepository: Repository<Enrolment>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Course)
+    private courseRepository: Repository<Course>,
+  ) {}
+
+  async create(createEnrolmentDto: CreateEnrolmentDto): Promise<Enrolment> {
+    const { userId, courseId } = createEnrolmentDto;
+
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const course = await this.courseRepository.findOne({
+      where: { id: courseId },
+    });
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+
+    const enrolmentExists = await this.enrolmentRepository.findOne({
+      where: { user, course },
+      withDeleted: true,
+    });
+    if (enrolmentExists) {
+      throw new Error('User already enrolled in this course');
+    }
+
+    const newEnrolment = await this.enrolmentRepository.save(
+      this.enrolmentRepository.create({
+        user,
+        course,
+      }),
+    );
+
+    return await this.enrolmentRepository.findOne({
+      where: { id: newEnrolment.id },
+      relations: ['user', 'course'],
+      loadRelationIds: true,
+    });
   }
 
-  findAll() {
-    return `This action returns all enrolments`;
+  async findAll(withDeleted: boolean = false): Promise<Enrolment[]> {
+    return await this.enrolmentRepository.find({
+      withDeleted,
+      relations: ['user', 'course'],
+      loadRelationIds: true,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} enrolment`;
+  async findOne(id: string): Promise<Enrolment> {
+    const enrolment = await this.enrolmentRepository.findOne({
+      where: { id },
+      relations: ['user', 'course'],
+      loadRelationIds: true,
+    });
+
+    if (!enrolment) {
+      throw new NotFoundException('Enrolment not found');
+    }
+
+    return enrolment;
   }
 
-  update(id: number, updateEnrolmentDto: UpdateEnrolmentDto) {
-    return `This action updates a #${id} enrolment`;
+  async update(
+    id: string,
+    updateEnrolmentDto: UpdateEnrolmentDto,
+  ): Promise<{ id: string }> {
+    const enrolment = await this.enrolmentRepository.findOne({
+      where: { id },
+    });
+
+    if (!enrolment) {
+      throw new NotFoundException('Enrolment not found');
+    }
+
+    const updatedEnrolment = await this.enrolmentRepository.update(id, {
+      ...enrolment,
+      ...updateEnrolmentDto,
+    });
+
+    if (updatedEnrolment.affected <= 0) {
+      throw new InternalServerErrorException('Enrolment not updated');
+    }
+
+    return { id };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} enrolment`;
+  async remove(id: string): Promise<{ id: string }> {
+    const enrolment = await this.enrolmentRepository.findOne({
+      where: { id },
+    });
+
+    if (!enrolment) {
+      throw new NotFoundException('Enrolment not found');
+    }
+
+    const updatedEnrolment = await this.enrolmentRepository.update(id, {
+      deleted_at: new Date(),
+    });
+
+    if (updatedEnrolment.affected <= 0) {
+      throw new InternalServerErrorException('Enrolment not deleted');
+    }
+
+    return { id };
   }
 }
