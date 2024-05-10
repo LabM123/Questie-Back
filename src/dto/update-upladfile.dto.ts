abe4eefe51d3e730a2203cd68bb09031bc86fa52
LabM <bcdld@hotmail.com>
2024-05-10 14:40:43 -0600

    fix/ erased some useless files

--- src/dto/update-upladfile.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUpladfileDto } from './create-upladfile.dto';
-
-export class UpdateUpladfileDto extends PartialType(CreateUpladfileDto) {}
