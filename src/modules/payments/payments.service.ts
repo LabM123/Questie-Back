43189dad74142ae3ff9f032a92563a6ddc949959
Joel Blanco <joeld.blanco@gmail.com>
2024-05-10 16:37:01 -0500

    feat: Implement payment functionality with Coins
    This commit implements the payment functionality with Coins in the Payments module. It adds a new endpoint `/coins` to the PaymentsController for making payments with Coins. The `payWithCoins` method in the PaymentsService now returns the `invoiceId` after making the payment. The `success` method in the PaymentsController has been updated to receive the `invoiceId` as a query parameter and calls the `success` method in the PaymentsService to update the invoice status to completed. The success endpoint now redirects to the URL of the paid invoice.

--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,10 +1,32 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PaypalService } from './paypal/paypal.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { User } from '../users/entities/user.entity';
+import { Course } from '../courses/entities/course.entity';
+import { Enrolment } from '../enrolments/entities/enrolment.entity';
+import { InvoicesService } from '../invoices/invoices.service';
 
 @Injectable()
 export class PaymentsService {
   constructor(
     @Inject(PaypalService) private readonly paypalService: PaypalService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Enrolment)
+    private readonly enrolmentRepository: Repository<Enrolment>,
+    @Inject(InvoicesService)
+    private readonly invoicesService: InvoicesService,
   ) {}
 
   async payWithMercadoPago({
@@ -34,10 +56,58 @@ export class PaymentsService {
     userId: string;
     productId: string;
   }) {
-    throw new Error('Method not implemented.');
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['stats', 'enrolments'],
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const product = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    const course = await this.courseRepository.findOne({
+      where: { id: product.polymorphicEntityId },
+    });
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+
+    const enrolmentExists = user.enrolments.find(
+      async (enrolment) => enrolment.course.id === course.id,
+    );
+    if (enrolmentExists) {
+      throw new ConflictException('User is already enrolled in this course');
+    }
+
+    if (user.stats.coins < product.price) {
+      throw new ForbiddenException('Not enough coins');
+    }
+
+    user.stats.coins -= product.price;
+    await this.userRepository.save(user);
+
+    await this.enrolmentRepository.save(
+      this.enrolmentRepository.create({
+        user,
+        course,
+      }),
+    );
+
+    const invoiceId = this.invoicesService.createInvoice(userId, productId);
+
+    return invoiceId;
   }
 
-  async success() {
-    throw new Error('Method not implemented.');
+  async success(invoiceId: string) {
+    const paidInvoiceId =
+      await this.invoicesService.updateToCompleted(invoiceId);
+
+    return paidInvoiceId;
   }
 }
