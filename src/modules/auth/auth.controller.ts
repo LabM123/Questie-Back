04775dfdb6d7cdc32a8873bea476c1db4994f8cc
zimmer95 <jorgezimmer95@gmail.com>
2024-05-11 12:08:47 -0300

    add api operation to controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,17 +2,19 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 @ApiTags("Auth")
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign in user', description: 'Allows a user to sign in by providing their credentials.' })
   @Post('signin')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.loginUser(loginUserDto);
   }
 
+  @ApiOperation({ summary: 'Sign up user', description: 'Allows a user to sign up by providing their registration details.' })
   @Post('signup')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.registerUser(registerUserDto);
