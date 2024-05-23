d0025d607b6f94e58d129064265ff5e4ab3a0602
Joel Blanco <joeld.blanco@gmail.com>
2024-05-23 09:30:46 -0500

    fix: Add user repository import to stats module

--- src/modules/stats/stats.module.ts
+++ src/modules/stats/stats.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StatsService } from './stats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [StatsService],
 })
 export class StatsModule {}
