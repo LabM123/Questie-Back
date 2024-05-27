d2311b4159d6057963de7056885d37870e5c7539
zimmer95 <jorgezimmer95@gmail.com>
2024-05-27 10:26:22 -0300

    add api decorators

--- src/modules/auth0/auth0.controller.ts
+++ src/modules/auth0/auth0.controller.ts
@@ -1,12 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/modules/users/dto/create-user.dto';
 import { Auth0Service } from './auth0.service';
 
+@ApiTags('Auth0')
 @Controller('auth0')
 export class Auth0Controller {
   constructor(private readonly auth0Service: Auth0Service) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Registrar un nuevo usuario en Auth0' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'El usuario ha sido registrado exitosamente.',
+  })
+  @ApiResponse({ status: 400, description: 'Datos de entrada inválidos.' })
   async registerUser(@Body() createUserDto: CreateUserDto) {
     return this.auth0Service.createAuth0User(createUserDto);
   }
--- src/modules/auth0/constant.ts
+++
@@ -1,5 +0,0 @@
-// auth/constants.ts
-export const jwtConstants = {
-    secret: process.env.JWT_SECRET || 'yourSecretKeyHere',  // Asegúrate de tener una variable de entorno JWT_SECRET configurada
-  };
-  
